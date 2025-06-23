62b7733dbdfef8bb68edc9a6793e99dc5577b094
Mingi Kang <rkdqlwla@gmail.com>
2025-06-23 21:26:28 +0900

    [update] add loader caching dict

--- randatoms/__init__.py
+++ randatoms/__init__.py
@@ -2,11 +2,11 @@ from .dataloader import DataLoader
 from .utils import available_datasets
 
 # High-level API for direct access
-_loader = None
+_loader_dict = {}
 
 def randomatoms(*args, filename='default', data_dir=None, **kwargs):
-    global _loader
-    if _loader is None:
-        _loader = DataLoader(filename=filename, data_dir=data_dir)
-    return _loader.get_random_structures(*args, **kwargs)
+    if not _loader_dict.get(filename, None):
+        _loader_dict[filename] = DataLoader(filename=filename, data_dir=data_dir)
+        
+    return _loader_dict[filename].get_random_structures(*args, **kwargs)
 
